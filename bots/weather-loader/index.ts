c4d6b3553b34b27886257bb84a514624b3fc1ba3
Clint Zirker <clint.zirker@dsco.io>
2022-05-12 10:24:08 -0600

    Cleaning up project

--- bots/weather-loader/index.ts
+++ bots/weather-loader/index.ts
@@ -3,8 +3,6 @@ import { BotInvocationEvent, RStreamsContext } from "leo-sdk";
 import botWrapper from "leo-sdk/wrappers/cron";
 import { LatLng, WeatherData } from "../../lib/types";
 import axios from "axios";
-import projConfig from "../../project-config-new";
-const configuration = projConfig; // Typescript renames projConfig because of the import which prevents inspecting when debugging
 
 
 interface LoaderBotInvocationEvent extends BotInvocationEvent {
@@ -14,7 +12,6 @@ interface LoaderBotInvocationEvent extends BotInvocationEvent {
 // botid: ${self:service}-weather-loader
 export const handler = botWrapper(async function (event: LoaderBotInvocationEvent, context: RStreamsContext) {
 	console.log("Invocation Event:", JSON.stringify(event, null, 2));
-	console.log(configuration.defaultNumRetries, configuration.weather.inner);
 
 	const weatherData = await getWeather({ lat: 40.35, lng: -111.90 });
 
